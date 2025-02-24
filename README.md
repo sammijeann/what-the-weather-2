be93decb53676834f3806e2d909de4a8c57c9a8c
Sammi Teel <sammijteel@gmail.com>
2025-02-24 11:50:11 -0600

    set-up code

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # what-the-weather-2
+
+
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+## Table of Contents
+- [License](#license)
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Testing](#testing)
+
+## License
+This project is licensed under the [MIT](https://opensource.org/licenses/MIT) license.
+
+## Description
+This project is an interactive weather app that utilizes the OpenWeatherApi to allow users to see the weather forecasts in different cities.
+
+## Installation
+Download the file and extract data
+
+## Usage
+Video Walkthrough: https://drive.google.com/file/d/17gjEOW8YIxivZ9kp1F5TpWt0ixe5pU5V/view?usp=sharing
+Open in an IDE (I used VS Code)
+
+## Contribution
+Keep credits in comments
+
+## Testing
+I do not have a test for this currently
+
+## Questions
+Feel free to reach out to me with any questions at sammijteel@gmail.com or on GitHub at https://github.com/sammijeann
---
+++ server/src/service/weatherService.ts
@@ -0,0 +1,119 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+// TODO: Define an interface for the Coordinates object
+interface Coordinates {
+  lat: number;
+  lon: number;
+}
+
+// TODO: Define a class for the Weather object
+class Weather {
+  tempF: number;
+  humidity: number;
+  windSpeed: number;
+  icon: string;
+  date?: string;
+  city?: string;
+  iconDescription?: string;
+
+  constructor() {
+    this.tempF = 0;
+    this.humidity = 0;
+    this.windSpeed = 0;
+    this.icon = '';
+    this.date = '';
+    this.city = '';
+    this.iconDescription = '';
+  }
+}
+
+// TODO: Complete the WeatherService class
+class WeatherService {
+
+  // TODO: Define the baseURL, API key, and city name properties
+  baseURL: string; 
+  weatherApi: string; 
+  cityName: string; 
+
+  constructor() {
+    this.baseURL = process.env.API_BASE_URL || '';
+    this.weatherApi = process.env.API_KEY || '';
+    this.cityName = ''; 
+}
+
+  // TODO: Create fetchLocationData method
+  private async fetchLocationData(query: string) {
+    const response = await fetch(`${this.baseURL}/geo/1.0/direct?q=${query}&limit=1&appid=${this.weatherApi}`);
+    const data = await response.json();
+    return data[0];
+  }
+
+  // TODO: Create destructureLocationData method
+  private destructureLocationData(locationData: Coordinates): Coordinates {
+    
+    return {
+      lat: locationData.lat,
+      lon: locationData.lon
+    };
+  }
+
+  // TODO: Create buildGeocodeQuery method
+  private buildGeocodeQuery(): string {
+    return `${this.baseURL}/geo/1.0/direct?q=${this.cityName}&limit=1&appid=${this.weatherApi}`;
+  }
+
+  // TODO: Create buildWeatherQuery method
+  private buildWeatherQuery(coordinates: Coordinates): string {
+    return `${this.baseURL}/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${this.weatherApi}&units=imperial`;
+  }
+
+  // TODO: Create fetchAndDestructureLocationData method
+  private async fetchAndDestructureLocationData(city: string) {
+    const locationData = await this.fetchLocationData(city);
+    return this.destructureLocationData(locationData);
+  }
+
+  // TODO: Create fetchWeatherData method
+  private async fetchWeatherData(coordinates: Coordinates) {
+    const response = await fetch(this.buildWeatherQuery(coordinates));
+    return await response.json();
+  }
+
+  // TODO: Build parseCurrentWeather method
+  private parseCurrentWeather(response: any) {
+    const weather = new Weather();
+    weather.tempF = response.main.temp;
+    weather.humidity = response.main.humidity;
+    weather.windSpeed = response.wind.speed;
+    weather.icon = response.weather[0].icon;
+    weather.iconDescription = response.weather[0].description;
+    weather.city = response.name;
+    return weather;
+  }
+
+  // TODO: Complete buildForecastArray method
+  private buildForecastArray(currentWeather: Weather, weatherData: any[]) {
+    const forecastArray: Weather[] = [];
+    weatherData.forEach((data: any) => {
+      const weather = new Weather();
+      weather.tempF = data.main.temp;
+      weather.humidity = data.main.humidity;
+      weather.windSpeed = data.wind.speed;
+      weather.icon = data.weather[0].icon;
+      weather.iconDescription = data.weather[0].description;
+      weather.date = data.dt_txt;
+      forecastArray.push(weather);
+    });
+    return forecastArray;
+  }
+
+  // TODO: Complete getWeatherForCity method
+  async getWeatherForCity(city: string) {
+    const coordinates = await this.fetchAndDestructureLocationData(city);
+    const weatherData = await this.fetchWeatherData(coordinates);
+    return this.parseCurrentWeather(weatherData);
+  }
+}
+
+export default new WeatherService();
